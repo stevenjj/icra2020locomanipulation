05d2a29db1dd7d694e47a3db7fb347eedcabbe50
Ryan Gupta <ryangupta8@gmail.com>
2019-07-09 18:24:26 -0500

    now we properly have shared_ptr to a RobotModel as data in collision environment

--- include/avatar_locomanipulation/collision_environment/collision_environment.h
+++ include/avatar_locomanipulation/collision_environment/collision_environment.h
@@ -8,25 +8,15 @@
 
 
 
-class Collision
+class CollisionEnvironment
 {
 private:
-	
-
+  std::shared_ptr<RobotModel> valkyrie, object;
 public:
-	Collision();
-	~Collision();
-	void build_valkyrie_model_and_geom();
-	void build_box_planar_joint_model_and_geom();
-	void build_cart_model_and_geom();
-	void append_models(std::shared_ptr<Collision> & parent, std::shared_ptr<Collision> & child, std::shared_ptr<Collision> & appended);// Parent, child
-	void set_configuration_vector(Eigen::VectorXd & config_vec);// vector of configurations
-	void compute_distances();
-	void compute_near_point(std::string name1, std::string name2, Eigen::Vector3d & near_point, Eigen::VectorXd q_start);
-	void compute_collision(); 
-	pinocchio::fcl::Quaternion3f makeQuat(double w, double x, double y, double z);
-	int get_nq();
-	void get_position_of_joints(Eigen::VectorXd q_start, std::map<std::string, Eigen::Vector3d> & positions);
+	
+  CollisionEnvironment(std::shared_ptr<RobotModel> & val, std::shared_ptr<RobotModel> & obj);
+  ~CollisionEnvironment();
+	
 };
 
 
--- src/avatar_locomanipulation/collision_environment/collision_environment.cpp
+++ src/avatar_locomanipulation/collision_environment/collision_environment.cpp
@@ -1,1 +1,9 @@
 #include <avatar_locomanipulation/collision_environment/collision_environment.h>
+
+
+
+CollisionEnvironment::CollisionEnvironment(std::shared_ptr<RobotModel> & val, std::shared_ptr<RobotModel> & obj){
+	valkyrie = val;
+	object = obj;
+	std::cout << "Collision Environment Created" << std::endl;
+}
