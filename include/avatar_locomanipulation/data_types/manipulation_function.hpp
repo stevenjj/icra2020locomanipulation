539f7e82745f139a6ef3d6f7c065ef9c58d0f6f2
Steven Jens Jorgensen <stevenjensj@gmail.com>
2019-07-30 16:04:28 -0500

    added transform to world coordinates for the manipulation function

--- include/avatar_locomanipulation/data_types/manipulation_function.hpp
+++ include/avatar_locomanipulation/data_types/manipulation_function.hpp
@@ -15,13 +15,25 @@ public:
   ManipulationFunction(std::string filename);
   ~ManipulationFunction(); 
 
+  void setWorldTransform(const Eigen::Vector3d & translation, const Eigen::Quaterniond & orientation);
   void getPose(double & s, Eigen::Vector3d & pos_out, Eigen::Quaterniond & quat_out);
   void setWaypointsFromYaml(std::string filename);
  
 private:
+  void common_initialization();
+
   std::shared_ptr<CubicInterpolationSixDimVec> f_s;
   std::string waypoint_list_yaml_filename;
   int N_waypoints;
+
+  // Local frame position and orientation of the interpolated waypoint
+  Eigen::Vector3d local_pos;
+  Eigen::Quaterniond local_ori;
+
+
+  // Homogenous transform w.r.t world frame to convert
+  Eigen::Vector3d transform_translation;
+  Eigen::Quaterniond transform_orientation;
 
 
 };
--- src/avatar_locomanipulation/data_types/manipulation_function.cpp
+++ src/avatar_locomanipulation/data_types/manipulation_function.cpp
@@ -1,12 +1,15 @@
 #include <avatar_locomanipulation/data_types/manipulation_function.hpp>
 
 
-ManipulationFunction::ManipulationFunction(){}
+ManipulationFunction::ManipulationFunction(){
+	common_initialization();	
+}
 ManipulationFunction::~ManipulationFunction(){}
 
 
 ManipulationFunction::ManipulationFunction(std::string filename){
 	setWaypointsFromYaml(filename);
+	common_initialization();
 }
 
 void ManipulationFunction::setWaypointsFromYaml(std::string filename){
@@ -15,7 +18,26 @@ void ManipulationFunction::setWaypointsFromYaml(std::string filename){
 	f_s.reset(new CubicInterpolationSixDimVec(filename));
 }
 
-void ManipulationFunction::getPose(double & s, Eigen::Vector3d & pos_out, Eigen::Quaterniond & quat_out){
-	f_s->getPose(s, pos_out, quat_out);
+
+void ManipulationFunction::setWorldTransform(const Eigen::Vector3d & translation, const Eigen::Quaterniond & orientation){
+	transform_translation = translation;
+	transform_orientation = orientation;
 }
 
+
+void ManipulationFunction::getPose(double & s, Eigen::Vector3d & pos_out, Eigen::Quaterniond & quat_out){
+	f_s->getPose(s, local_pos, local_ori);
+
+	// Transform to world coordinates
+	pos_out = transform_orientation.toRotationMatrix()*local_pos + transform_translation;
+	quat_out = transform_orientation*local_ori;
+}
+
+
+void ManipulationFunction::common_initialization(){
+	local_pos.setZero();
+	local_ori.setIdentity();
+
+	transform_translation.setZero();
+	transform_orientation.setIdentity();
+}
