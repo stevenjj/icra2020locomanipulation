c914f48f71712472e6cea7729734aff4e96bd754
Steven Jens Jorgensen <stevenjensj@gmail.com>
2019-08-15 14:15:32 -0500

    added generate folders script

---
+++ scripts/generate_folders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+num=3
+parent_folder="param_set_$num"
+
+echo "Creating folders: $parent_folder/raw_positive_initial_config_data"
+echo "Creating folders: $parent_folder/raw_positive_transitions_data"
+echo "Creating folders: $parent_folder/transitions_data_with_task_space_info/negative_examples"
+echo "Creating folders: $parent_folder/transitions_data_with_task_space_info/positive_examples"
+
+mkdir -p "$parent_folder/raw_positive_initial_config_data"
+mkdir -p "$parent_folder/raw_positive_transitions_data"
+mkdir -p "$parent_folder/transitions_data_with_task_space_info/negative_examples"
+mkdir -p "$parent_folder/transitions_data_with_task_space_info/positive_examples"
