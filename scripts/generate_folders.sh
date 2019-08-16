6f29e60d7c3361734207c2c37ffe0ac98d01410c
stevenjj <stevenjensj@gmail.com>
2019-08-15 21:19:35 -0500

    modify creation of subfolders. specific launch files for contact transition types

--- scripts/generate_folders.sh
+++ scripts/generate_folders.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-num=10
-parent_folder="param_set_$num/both_hands"
+num=1
+parent_folder="/home/$USER/Data/param_set_$num"
 
-echo "Creating folders: $parent_folder/raw_positive_initial_config_data"
-echo "Creating folders: $parent_folder/raw_positive_transitions_data"
-echo "Creating folders: $parent_folder/transitions_data_with_task_space_info/negative_examples"
-echo "Creating folders: $parent_folder/transitions_data_with_task_space_info/positive_examples"
+create_subfolders(){
+	echo "Creating folders: $1/$2/raw_positive_initial_config_data"
+	echo "Creating folders: $1/$2/raw_positive_transitions_data"
+	echo "Creating folders: $1/$2/transitions_data_with_task_space_info/negative_examples"
+	echo "Creating folders: $1/$2/transitions_data_with_task_space_info/positive_examples"
 
-mkdir -p "$parent_folder/raw_positive_initial_config_data"
-mkdir -p "$parent_folder/raw_positive_transitions_data"
-mkdir -p "$parent_folder/transitions_data_with_task_space_info/negative_examples"
-mkdir -p "$parent_folder/transitions_data_with_task_space_info/positive_examples"
+	mkdir -p "$1/$2/raw_positive_initial_config_data"
+	mkdir -p "$1/$2/raw_positive_transitions_data"
+	mkdir -p "$1/$2/transitions_data_with_task_space_info/negative_examples"
+	mkdir -p "$1/$2/transitions_data_with_task_space_info/positive_examples"
+}
+
+
+create_subfolders $parent_folder "both_hands"
+create_subfolders $parent_folder "right_hand"
+create_subfolders $parent_folder "left_hand"
+
