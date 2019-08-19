625731bee16085d9fc8f8b6363ffbc7f3462f0b6
Steven Jens Jorgensen <stevenjensj@gmail.com>
2019-08-19 16:28:11 -0500

    sets x and y training data values. numerical representation for stance and manipulation type

--- src/python_src/load_training_data.py
+++ src/python_src/load_training_data.py
@@ -16,22 +16,25 @@ def convert_quat_to_3vec(quat):
 	# Compute theta
 	theta = np.arccos(w)
 
-	print theta
 	if (theta <= 1e-6):
 		return np.array([0.0, 0.0, 0.0])
 	else:
 		vec = np.array([x,y,z]) / np.sin(theta)
-		print vec
 		vec = vec / np.linalg.norm(vec) * theta
-		print vec
 		return vec 
 
 class TransitionData:
 	def __init__(self):
+		self.stance_origin_to_num = { "left_foot": 0, "right_foot":1 }
+		self.manipulation_type_to_num = { "left_hand": 0, "right_hand":1, "both_hands":2 }
+		
 		self.path = ""
 
 		self.stance_origin = ""
+		self.stance_origin_num = 0
+
 		self.manipulation_type = ""
+		self.manipulation_type_num = 0
 		self.result = 0
 
 		self.swing_foot_starting_position = np.array([0,0,0], dtype='f') #x, y, z
@@ -54,6 +57,24 @@ class TransitionData:
 		self.landing_foot_orientation_quat = np.array([0,0,0,0], dtype='f')
 		self.landing_foot_orientation_vec =  np.array([0,0,0], dtype='f') #rx,ry,rz
 
+		self.set_x()
+		self.set_y()
+
+	def set_x(self):
+		# sets the x vector for the training data
+		self.x = np.concatenate( (self.swing_foot_starting_position, self.swing_foot_starting_orientation_vec,
+								  self.pelvis_starting_position, self.pelvis_starting_orientation_vec,
+								  self.left_hand_starting_position,self.left_hand_starting_orientation_vec,
+								  self.right_hand_starting_position,self.right_hand_starting_orientation_vec,
+								  self.landing_foot_position, self.landing_foot_orientation_vec) )
+	def set_y(self):
+		self.y = self.result
+
+	def get_x(self):
+		return self.x
+
+	def get_y(self):
+		return self.y
 
 	def loadPosition(self, data_loaded, key, pos):
 		pos[0] = data_loaded[key]["x"]
@@ -66,6 +87,10 @@ class TransitionData:
 		ori[2] = data_loaded[key]["z"]	
 		ori[3] = data_loaded[key]["w"]			
 
+	def loadYamlFile(self, yaml_file_path):
+		with open(filepath, 'r') as stream:
+			data_loaded = yaml.load(stream)		
+			self.loadData(data_loaded)
 
 	def loadData(self, data_loaded):
 		self.path = filepath 
@@ -73,6 +98,9 @@ class TransitionData:
 		self.stance_origin = data_loaded["stance_origin"] 
 		self.manipulation_type = data_loaded["manipulation_type"]		
 		self.result = 1 if data_loaded["result"] == "success" else 0
+
+		self.stance_origin_num = self.stance_origin_to_num[self.stance_origin]
+		self.manipulation_type_num = self.manipulation_type_to_num[self.manipulation_type]
 
 		self.loadPosition(data_loaded, "swing_foot_starting_position", self.swing_foot_starting_position)
 		self.loadOrientation(data_loaded, "swing_foot_starting_orientation", self.swing_foot_starting_orientation_quat)
@@ -94,33 +122,35 @@ class TransitionData:
 		self.loadOrientation(data_loaded, "landing_foot_orientation", self.landing_foot_orientation_quat)
 		self.landing_foot_orientation_vec = convert_quat_to_3vec(self.landing_foot_orientation_quat)
 
+		self.set_x()
+		self.set_y()
 
 	def printData(self):
 		print "yaml file:", self.path
 		print " "
-		print "  stance_origin", self.stance_origin
-		print "  manipulation_type" , self.manipulation_type
-		print "  result" , self.result
+		print "  stance_origin:", self.stance_origin, ", num representation = ", self.stance_origin_num
+		print "  manipulation_type:" , self.manipulation_type, ", num representation = ",  self.manipulation_type_num
+		print "  result:" , self.result
 		print " "
-		print "  swing_foot_starting_position" , self.swing_foot_starting_position
-		print "  swing_foot_starting_orientation_quat" , self.swing_foot_starting_orientation_quat
-		print "  swing_foot_starting_orientation_vec" , self.swing_foot_starting_orientation_vec
+		print "  swing_foot_starting_position:" , self.swing_foot_starting_position
+		print "  swing_foot_starting_orientation_quat:" , self.swing_foot_starting_orientation_quat
+		print "  swing_foot_starting_orientation_vec:" , self.swing_foot_starting_orientation_vec
 		print " "
-		print "  pelvis_starting_position" , self.pelvis_starting_position
-		print "  pelvis_starting_orientation_quat" , self.pelvis_starting_orientation_quat
-		print "  pelvis_starting_orientation_vec" , self.pelvis_starting_orientation_vec
+		print "  pelvis_starting_position:" , self.pelvis_starting_position
+		print "  pelvis_starting_orientation_quat:" , self.pelvis_starting_orientation_quat
+		print "  pelvis_starting_orientation_vec:" , self.pelvis_starting_orientation_vec
 		print " "
-		print "  left_hand_starting_position" , self.left_hand_starting_position
-		print "  left_hand_starting_orientation_quat" , self.left_hand_starting_orientation_quat
-		print "  left_hand_starting_orientation_vec" , self.left_hand_starting_orientation_vec
+		print "  left_hand_starting_position:" , self.left_hand_starting_position
+		print "  left_hand_starting_orientation_quat:" , self.left_hand_starting_orientation_quat
+		print "  left_hand_starting_orientation_vec:" , self.left_hand_starting_orientation_vec
 		print " "
-		print "  right_hand_starting_position" , self.right_hand_starting_position
-		print "  right_hand_starting_orientation_quat" , self.right_hand_starting_orientation_quat
-		print "  right_hand_starting_orientation_vec" , self.right_hand_starting_orientation_vec
+		print "  right_hand_starting_position:" , self.right_hand_starting_position
+		print "  right_hand_starting_orientation_quat:" , self.right_hand_starting_orientation_quat
+		print "  right_hand_starting_orientation_vec:" , self.right_hand_starting_orientation_vec
 		print " "
-		print "  landing_foot_position" , self.landing_foot_position
-		print "  landing_foot_orientation_quat" , self.landing_foot_orientation_quat
-		print "  landing_foot_orientation_vec" , self.landing_foot_orientation_vec
+		print "  landing_foot_position:" , self.landing_foot_position
+		print "  landing_foot_orientation_quat:" , self.landing_foot_orientation_quat
+		print "  landing_foot_orientation_vec:" , self.landing_foot_orientation_vec
 
 
 def load_file(filepath):
@@ -131,6 +161,8 @@ def load_file(filepath):
 
 	training_data.loadData(data_loaded)
 	training_data.printData()
+
+	print "x vec = ", training_data.get_x()
 
 
 
