e13103822276f7660344ad7934689abdb34046f5
Steven Jens Jorgensen <stevenjensj@gmail.com>
2019-08-19 10:54:42 -0500

    also print out number of negative examples

--- scripts/print_positive_example_count.sh
+++ scripts/print_positive_example_count.sh
@@ -20,6 +20,19 @@ count_data "$parent_folder/left_hand/transitions_data_with_task_space_info/posit
 count_data "$parent_folder/both_hands/transitions_data_with_task_space_info/positive_examples" "both_hands_right_foot"
 count_data "$parent_folder/both_hands/transitions_data_with_task_space_info/positive_examples" "both_hands_left_foot"
 
+
+echo " "
+echo "Number of Negative Examples Generated"
+
+count_data "$parent_folder/right_hand/transitions_data_with_task_space_info/negative_examples" "right_hand_right_foot"
+count_data "$parent_folder/right_hand/transitions_data_with_task_space_info/negative_examples" "right_hand_left_foot"
+
+count_data "$parent_folder/left_hand/transitions_data_with_task_space_info/negative_examples" "left_hand_right_foot"
+count_data "$parent_folder/left_hand/transitions_data_with_task_space_info/negative_examples" "left_hand_left_foot"
+
+count_data "$parent_folder/both_hands/transitions_data_with_task_space_info/negative_examples" "both_hands_right_foot"
+count_data "$parent_folder/both_hands/transitions_data_with_task_space_info/negative_examples" "both_hands_left_foot"
+
 echo " "
 echo " Dataset file size: " `du -hs $parent_folder`
 
