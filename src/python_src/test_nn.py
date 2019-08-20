b192a1a5e228809798e00adf9fcc624027dd8887
Steven Jens Jorgensen <stevenjensj@gmail.com>
2019-08-19 19:09:35 -0500

    evaluating neural network with test set as well

--- src/python_src/test_nn.py
+++ src/python_src/test_nn.py
@@ -5,21 +5,38 @@ from tensorflow.keras import layers
 
 import contact_transition_dataset as transition_dataset
 
+import numpy as np
+
 print(tf.version.VERSION)
 print(tf.keras.__version__)
 
 # Load data
 dataset_folder = "/home/sjorgen1/Data/param_set_1/right_hand/transitions_data_with_task_space_info"
-dataset = transition_dataset.ContactTransitionDataset(20)
+total_data = 200
+dataset = transition_dataset.ContactTransitionDataset(total_data/2)
 dataset.load_dataset(dataset_folder)
 
 # Get the normalize the data
-(x_train, y_train) = dataset.get_normalized_xy_train()
+(x_dataset, y_dataset) = dataset.get_normalized_xy_train()
+(x_dataset_rand, y_dataset_rand) = dataset.get_normalized_xy_train()
+dataset_dim = x_dataset.shape[1] 
 
-print x_train
-print y_train
+# Randomize the data
+random_indices = np.random.choice(x_dataset.shape[0], x_dataset.shape[0], replace=False)
+for i in range(0, len(random_indices)):
+	x_dataset_rand[i] = x_dataset[random_indices[i]]
+	y_dataset_rand[i] = y_dataset[random_indices[i]]
 
-dataset_dim = x_train.shape[1] 
+trainset_size = int(total_data*0.8)
+
+
+# Set the training set and test set
+x_train = x_dataset_rand[:trainset_size]
+y_train = y_dataset_rand[:trainset_size]
+
+x_test = x_dataset_rand[trainset_size:]
+y_test = y_dataset_rand[trainset_size:]
+
 
 # Construct Sequential Model
 model = tf.keras.Sequential()
@@ -41,10 +58,11 @@ model.compile(optimizer=tf.train.AdamOptimizer(0.001),
 
 # Fit
 model.fit(x_train, y_train, epochs=50, batch_size=32)
+
 # Evaluate
-model.evaluate(x_train, y_train)
+print 
+print "Evaluating test set:"
+model.evaluate(x_test, y_test)
 
-# model.add( layers.Dense(64, activation='relu', input_shape( dim ,)) )
 
-print "prediction for ", x_train[:1]
-print model.predict(x_train[:1])
+# print model.predict(x_train[:])
