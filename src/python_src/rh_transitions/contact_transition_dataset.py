40620002feb3268b9a4bb40643e7e378671c1468
Steven Jens Jorgensen <stevenjensj@gmail.com>
2019-08-21 13:55:34 -0500

    contact transition dataset is now processing the val and test sets

--- src/python_src/rh_transitions/contact_transition_dataset.py
+++ src/python_src/rh_transitions/contact_transition_dataset.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import copy
 
 import numpy as np
 
@@ -14,11 +15,15 @@ dataset_folder = "/home/sjorgen1/Data/param_set_1/right_hand/transitions_data_wi
 
 
 class ContactTransitionDataset:
-    def __init__(self, num_in = 100):
+    def __init__(self, num_in = 100, trainset_percentage_in=0.8, valset_percentage_in=0.2):
         self.num = num_in # Total number of training data
 
         self.x = np.array([])
         self.y = np.array([])
+
+        self.trainset_percentage = trainset_percentage_in
+        self.valset_percentage = valset_percentage_in
+        self.testset_percentage = 1.0 - trainset_percentage_in - valset_percentage_in
 
         self.x_train_mean = None
         self.x_train_std = None
@@ -32,6 +37,11 @@ class ContactTransitionDataset:
         self.x_test = None
         self.y_test = None
 
+        # Normalized datasets
+        self.x_train_normalized = None
+        self.x_val_normalized = None
+        self.x_test_normalized = None
+
         # contact transition data
         self.obj = datum.ContactTransitionData()
 
@@ -40,8 +50,8 @@ class ContactTransitionDataset:
         yaml_files = os.listdir(filepath)
         count = 0
 
-        print "Randomizing data load order..."
-        random_indices = np.random.choice(len(yaml_files), len(yaml_files), replace=False)
+        # print "Randomizing data load order..."
+        # random_indices = np.random.choice(len(yaml_files), len(yaml_files), replace=False)
         # for i in range(0, len(random_indices)):
         for i in range(len(yaml_files)):
             if (count < max_load):
@@ -56,19 +66,6 @@ class ContactTransitionDataset:
             else:
                 break
 
-
-        # for file in yaml_files:
-        #     if (count < max_load):
-        #         # Load the yaml file and add the training data
-        #         if self.obj.load_yaml_file(filepath + "/" + file, stance_type, manipulation_type):
-        #             print "Loading", count+1 , "/", max_load,  filepath, "/", file 
-        #             # self.obj.printData()
-        #             data_x.append(self.obj.get_x())
-        #             data_y.append(self.obj.get_y())
-        #             # increment counter
-        #             count += 1
-        #     else:
-        #         break
 
     def load_dataset(self, filepath, stance_type=None, manipulation_type=None):
         print os.listdir(filepath)
@@ -93,23 +90,64 @@ class ContactTransitionDataset:
         print "self.x.shape", self.x.shape
 
 
+    def prepare_and_normalize_dataset(self):
+        self.prepare_dataset()
         self.normalize_dataset()
+
+    def prepare_dataset(self):
+        x_dataset_rand, y_dataset_rand = copy.deepcopy(self.get_xy())
+        # Randomize the data
+        random_indices = np.random.choice(x_dataset_rand.shape[0], x_dataset_rand.shape[0], replace=False)
+        for i in range(0, len(random_indices)):
+            x_dataset_rand[i] = self.x[random_indices[i]]
+            y_dataset_rand[i] = self.y[random_indices[i]]
+
+        self.x = x_dataset_rand
+        self.y = y_dataset_rand
+
+        n_train = int(self.trainset_percentage*len(self.x))
+        n_val = int(self.valset_percentage*len(self.x))
+        n_test = len(self.x) - n_train - n_val
+
+        self.x_train = self.x[:n_train]
+        self.y_train = self.y[:n_train]
+
+        self.x_val = self.x[n_train : n_train + n_val]
+        self.y_val = self.y[n_train : n_train + n_val]
+
+        self.x_test = self.x[n_train + n_val :]
+        self.y_test = self.y[n_train + n_val :]
 
 
     def normalize_dataset(self):
         # compute mean and standard deviation
-        self.x_train_mean = np.mean(self.x, axis = 0)
-        x_train_std = np.std(self.x, axis = 0)
+        self.x_train_mean = np.mean(self.x_train, axis = 0)
+        x_train_std = np.std(self.x_train, axis = 0)
         # set std to 1.0 if std is near 0:       
         eps = 1e-6
         self.x_train_std = np.array( [elem if abs(elem) >= eps else 1.0 for elem in x_train_std], dtype='f' )
 
-        self.x_train = (self.x - self.x_train_mean) / self.x_train_std
-
-        self.y_train = self.y
+        # Normalize the training data
+        if (len(self.x_train) > 0):
+            self.x_train_normalized = (self.x_train - self.x_train_mean) / self.x_train_std
+        
+        if (len(self.x_val) > 0):
+            self.x_val_normalized = (self.x_val - self.x_train_mean) / self.x_train_std
+        
+        if (len(self.x_test) > 0):
+            self.x_test_normalized = (self.x_test - self.x_train_mean) / self.x_train_std
 
     def get_normalized_xy_train(self):
-        return (self.x_train, self.y_train)
+        return (self.x_train_normalized, self.y_train)
+
+    def get_normalized_xy_val(self):
+        return (self.x_val_normalized, self.y_val)
+
+    def get_normalized_xy_test(self):
+        return (self.x_test_normalized, self.y_test)
+
+    def get_xy(self):
+        return (self.x, self.y)
 
 
     def enable_right_hand_data(self, bool_input):
