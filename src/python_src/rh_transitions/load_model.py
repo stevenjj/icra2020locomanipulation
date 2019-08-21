3eae006ecccee707e5d3aa5c061a5d73c10cd297
Steven Jens Jorgensen <stevenjensj@gmail.com>
2019-08-21 13:55:01 -0500

    loading normalization values now

--- src/python_src/rh_transitions/load_model.py
+++ src/python_src/rh_transitions/load_model.py
@@ -7,6 +7,7 @@ from tensorflow.keras import layers
 import contact_transition_dataset as transition_dataset
 
 import os
+import yaml
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,15 +15,16 @@ print(tf.version.VERSION)
 print(tf.keras.__version__)
 
 num_positive_data = 100
-contact_transition_types = [ ("left_hand", "right_foot") ]
+contact_transition_types = [ ("right_hand", "right_foot") ]
 shorthand = {"right_hand" : "rh", "left_hand" : "lh", "both_hands" : "bh", "right_foot": "rf", "left_foot": "lf"}
 
-def get_data(contact_transition_types_input):
-	dataset_folder = "/home/sjorgen1/Data/param_set_1/"
-	dataset = transition_dataset.ContactTransitionDataset(num_positive_data)
-	dataset.enable_right_hand_data(False)
-	dataset.enable_left_hand_data(True)
-	dataset.enable_stance_origin_data(False)
+
+def load_normalization_params(yaml_path):
+    with open(yaml_path, 'r') as stream:
+        data_loaded = yaml.load(stream)	
+    return np.array(data_loaded['x_train_mean'], dtype='f'), np.array(data_loaded['x_train_std'], dtype='f')
+
+def get_data(dataset, dataset_folder, contact_transition_types_input):
 
 	for type in contact_transition_types_input:
 		subfolder = "/transitions_data_with_task_space_info"
@@ -31,17 +33,10 @@ def get_data(contact_transition_types_input):
 	# Count total transition types
 	num_transition_types = len(contact_transition_types_input)
 
-	# Get the normalize thefilepath + "/" + data
-	(x_dataset, y_dataset) = dataset.get_normalized_xy_train()
-	(x_dataset_rand, y_dataset_rand) = dataset.get_normalized_xy_train()
+	# Get the raw data
+	(x_dataset, y_dataset) = dataset.get_xy()
 
-	# Randomize the data
-	random_indices = np.random.choice(x_dataset.shape[0], x_dataset.shape[0], replace=False)
-	for i in range(0, len(random_indices)):
-		x_dataset_rand[i] = x_dataset[random_indices[i]]
-		y_dataset_rand[i] = y_dataset[random_indices[i]]
-
-	return x_dataset_rand, y_dataset_rand
+	return x_dataset, y_dataset
 
 
 # Create a simple dense network for binary classification
@@ -66,18 +61,26 @@ def create_model(dataset_dim, num_hidden_layers = 3, units_per_layer=64, l2_reg=
 	return model
 
 # Get the data
-x_test, y_test = get_data(contact_transition_types)
+dataset_folder = "/home/sjorgen1/Data/param_set_1/"
+dataset = transition_dataset.ContactTransitionDataset(num_positive_data)
+x_test, y_test = get_data(dataset, dataset_folder, contact_transition_types)
 dataset_dim = x_test.shape[1]
 
-# Load the model
-save_folder = ""
+# Identify the load path
+load_folder = ""
 for type in contact_transition_types:
-	save_folder = save_folder + shorthand[type[0]] + "_" + shorthand[type[1]]
+	load_folder = load_folder + shorthand[type[0]] + "_" + shorthand[type[1]]
+load_path = "./learned_model/" + load_folder + "/"
 
+# Load the normalization parameters
+x_train_mean, x_train_std = load_normalization_params(load_path+'normalization_params.yaml')
+# Normalize the dataset
+x_test = (x_test - x_train_mean) / x_train_std
+
+# Load and create the model
 model = create_model(dataset_dim, num_hidden_layers=5, units_per_layer=256, l2_reg=0.001)
-model.load_weights("./learned_model/" + save_folder + "/")
+model.load_weights(load_path)
 print model.summary()
-
 
 # Evaluate
 print "Evaluating test set:"
