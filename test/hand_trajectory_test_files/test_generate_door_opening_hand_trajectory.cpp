f4e6546dd13130f2b8d526428da499f4be9d6d74
Ryan Gupta <ryangupta8@gmail.com>
2019-09-10 21:44:13 -0500

    added door opening trajectory. This file uses the x hand position and handle height but assumes that the y hand position has been made to match the original handle position. As with the bag lift, this is all parameterized using a yaml file

--- test/hand_trajectory_test_files/test_generate_door_opening_hand_trajectory.cpp
+++ test/hand_trajectory_test_files/test_generate_door_opening_hand_trajectory.cpp
@@ -1,4 +1,3 @@
-// Package Path Definition
 #include <Configuration.h>
 
 // Import ROS and Rviz visualization
@@ -39,17 +38,17 @@ void emit_7dof(YAML::Emitter & out, std::string & waypoint_name, std::vector<dou
 int main(int argc, char **argv){
 	ParamHandler param_handler;
 
-	param_handler.load_yaml_file(THIS_PACKAGE_PATH"hand_trajectory/bag_lift_putdown_parameters.yaml");
+	param_handler.load_yaml_file(THIS_PACKAGE_PATH"hand_trajectory/door_opening_parameters.yaml");
 
 	// Get our Configuration parameters
 	double r; // radius of arc
 	param_handler.getValue("radius", r);
 
 	double theta;
-	param_handler.getValue("rotation_angle", theta);
+	param_handler.getValue("door_open_angle", theta);
 
 	double l;
-	param_handler.getValue("lift_height", l);
+	param_handler.getValue("handle_height", l);
 
 	double pose_spacing;
 	param_handler.getValue("pose_spacing", pose_spacing);
@@ -57,20 +56,18 @@ int main(int argc, char **argv){
 	double arc_length = theta * r;
 	int N = static_cast<int>(arc_length / pose_spacing);// Number of wps to describe the arc
 
-  	int M = static_cast<int>(l / pose_spacing); // Number of wps to describe each of the up and down motions
-	
-  	double rx, ry, rz, rw;
+	double rx, ry, rz, rw;
 
   	param_handler.getNestedValue({"hand_initial_orientation", "x"}, rx);
   	param_handler.getNestedValue({"hand_initial_orientation", "y"}, ry);
   	param_handler.getNestedValue({"hand_initial_orientation", "z"}, rz);
   	param_handler.getNestedValue({"hand_initial_orientation", "w"}, rw);
 
-	Eigen::Vector3d hand_init;
-	hand_init[0] = 0.0; hand_init[1] = 0.0; hand_init[2] = 0.75;
+  	Eigen::Vector3d hand_init;
+	hand_init[0] = 0.0; hand_init[1] = 0.0; hand_init[2] = 0.0;
 	Eigen::Quaternion<double> q_init;
-	q_init.x() = 0.11781; q_init.y() = 0.39669;
-	q_init.z() = 0.60298; q_init.w() = 0.68204;
+	q_init.x() = rx; q_init.y() = ry;
+	q_init.z() = rz; q_init.w() = rw;
 	q_init.normalize();
 
 	// Helper variables
@@ -84,30 +81,18 @@ int main(int argc, char **argv){
 
 	// Begin our emitter
 	YAML::Emitter out;
-  out << YAML::BeginMap;
+  	out << YAML::BeginMap;
 
-	// When l!=0 we have 5 additional waypoints for the length
-	data_saver::emit_value(out, "num_waypoints", (2+M)+N+1);
-	// Generate waypoints for the line upwards 
-	for(int i=0; i<M; ++i){
+  	data_saver::emit_value(out, "num_waypoints", N+1);
+
+  	// Generate waypoints for the arc
+	for(int i=0; i<=N; ++i){
 		waypoint_string = "waypoint_" + std::to_string(i+1);
 
-		// x position = original_x + i*dl
-		values[0] = hand_init[0] + r;
-		values[1] = hand_init[1];
-		values[2] = hand_init[2] + i*dl;
-		values[3] = q_init.x(); values[4] = q_init.y(); values[5] = q_init.z(); values[6] = q_init.w();
-
-		emit_7dof(out, waypoint_string, values);
-	}
-	// Generate waypoints for the arc
-	for(int i=0; i<=N; ++i){
-		waypoint_string = "waypoint_" + std::to_string(i+M+1);
-
 		// x position = original_x + cos(i*dtheta)*radius
-		values[0] = hand_init[0] + cos(static_cast<double>(i)*dtheta)*r;
+		values[0] = hand_init[0] + cos(1.57 + static_cast<double>(i)*dtheta)*r;
 		// y position = original_y + cos(i*dtheta)*radius
-		values[1] = hand_init[1] + sin(static_cast<double>(i)*dtheta)*r;
+		values[1] = sin(1.57 +  static_cast<double>(i)*dtheta)*r;
 		// z position = original_z
 		values[2] = hand_init[2] + l;
 		// Get the updated quaternion
@@ -118,21 +103,8 @@ int main(int argc, char **argv){
 
 		emit_7dof(out, waypoint_string, values);
 	}
-  // Generate waypoints for the line downwards
-  for(int i=0; i<M; ++i){
-      waypoint_string = "waypoint_" + std::to_string(i+M+N+1);
 
-      // x position = original_x + i*dl
-      values[0] = hand_init[0] + cos(theta)*r;
-      values[1] = hand_init[1] + sin(theta)*r;
-      values[2] = hand_init[2] + l - i*dl;
-      values[3] = q_i.x(); values[4] = q_i.y(); values[5] = q_i.z(); values[6] = q_i.w();
-
-      emit_7dof(out, waypoint_string, values);
-    }
-
-
-  out << YAML::EndMap;
-	std::ofstream file_output_stream("bag_lift_turn_putdown_trajectory.yaml");
-  file_output_stream << out.c_str();
+	out << YAML::EndMap;
+	std::ofstream file_output_stream("door_open_trajectory.yaml");
+  	file_output_stream << out.c_str();
 }
