13418750ccf41a6c61ba2666ab47d370a0fb3107
Ryan Gupta <ryangupta8@gmail.com>
2019-09-12 08:50:28 -0500

    Now, chaning the door_opening_parameters.yaml file will be the sole place to make changes to the door waypoints. The door visualization follows anywhere. The limitation is that the fixed_Frame_orientation can only be rotated about the z axis. However, this is overcome by the fact that the transform b/w this hinge/fixed frame and the waypoints can be anything. In addition to the z rotation, the position of the hinge frame can be translated anywhere. I believe the door waypoints and visualization is in a ready state

--- src/avatar_locomanipulation/visualization_nodes/door_visualization_node.cpp
+++ src/avatar_locomanipulation/visualization_nodes/door_visualization_node.cpp
@@ -52,24 +52,33 @@ void DoorVisualizationNode::getParameters(){
 	q_init.x() = rx; q_init.y() = ry; q_init.z() = rz; q_init.w() = rw;
 	q_init.normalize();
 
-	q_fixed.x() = 0.; q_fixed.y() = 0.; q_fixed.z() = 0.; q_fixed.w() = 1.;
+	param_handler.getNestedValue({"hinge_fixed_frame_orientation", "x"}, rx);
+	param_handler.getNestedValue({"hinge_fixed_frame_orientation", "y"}, ry);
+	param_handler.getNestedValue({"hinge_fixed_frame_orientation", "z"}, rz);
+	param_handler.getNestedValue({"hinge_fixed_frame_orientation", "w"}, rw);
+
+	q_fixed.x() = rx; q_fixed.y() = ry; q_fixed.z() = rz; q_fixed.w() = rw;
 	q_fixed.normalize();
 }
 
 
 void DoorVisualizationNode::getVizInformation(visualization_msgs::Marker & door_msg, tf::Transform & tf_world_hinge){
 
-	Eigen::Quaternion<double> q, q_i;
+	Eigen::Quaternion<double> q, q_i, q_door_wrt_hinge;
+
+	q_door_wrt_hinge.x() = 0.0; q_door_wrt_hinge.y() = 0.0;
+	q_door_wrt_hinge.z() = 0.0; q_door_wrt_hinge.w() = 1.0;
 
 	tf_world_hinge.setOrigin(tf::Vector3(hinge_frame_pos[0], hinge_frame_pos[1], hinge_frame_pos[2]));
 	tf_world_hinge.setRotation(tf::Quaternion(q_fixed.x(), q_fixed.y(), q_fixed.z(), q_fixed.w()));
 
 	q.x() = 0.0; q.y() = 0.0; q.z() = sin((s_current*door_open_angle) / 2); q.w() = cos((s_current*door_open_angle) / 2);
-	
-	q_i = q * q_fixed;
+
+	q_i = (q * q_door_wrt_hinge);
+
 	// Fill the Door marker msg
-	door_msg.pose.position.x = -(radius / 2.) * sin(s_current * door_open_angle);;
-	door_msg.pose.position.y = (radius / 2.) * cos(s_current * door_open_angle); 
+	door_msg.pose.position.x = (radius / 2.) * cos(1.57 + (s_current * door_open_angle));//sin(s_current * door_open_angle);;
+	door_msg.pose.position.y = (radius / 2.) * sin(1.57 +  (s_current * door_open_angle));//cos(s_current * door_open_angle); 
 	door_msg.pose.position.z = 0.0;
 	door_msg.pose.orientation.x = q_i.x();
 	door_msg.pose.orientation.y = q_i.y();
